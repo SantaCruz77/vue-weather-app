fcdb9bb3186c68a43e568c1e09e3a6aa675b961d
SantaCruz77 <steveyamada66@gmail.com>
2023-03-22 07:35:00 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {reactive} from "vue"
+import axios from "axios"
+import Title from './components/Title.vue';
+import Form from './components/Form.vue';
+import Results from './components/Results.vue';
+// import "./assets/base.css"
+
+const results = reactive({
+  country:        "",
+  cityName:      "",
+  temperature:   "",
+  conditionText: "",
+  icon:          ""
+})
+
+const getWeather = (city) => {
+  axios.get(`https://api.weatherapi.com/v1/current.json?key=1ebb115188054f369fa11205232103&q=${city}&aqi=no`).then(res => {
+    results.country = res.data.location.country,
+    results.cityName = res.data.location.name,
+    results.temperature = res.data.current.temp_c,
+    results.conditionText = res.data.current.condition.text,
+    results.icon = res.data.current.condition.icon
+  })
+}
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <Title />
+      <Form @submit-form="getWeather" />
+      <Results :results="results"/>
+    </div>
+  </div>
+</template>
---
+++ src/components/Title.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Vue World Weather</h1>
+</template>
+<script setup></script>
+<style></style>
